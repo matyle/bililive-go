62002c1a1db91abef06fed69a2573bc9b45fab38
MatyTan <1396568121@qq.com>
2023-07-22 23:41:03 +0800

    fix:go forloop

--- src/cmd/bililive/bililive.go
+++ src/cmd/bililive/bililive.go
@@ -136,7 +136,9 @@ func main() {
 
 	//TODO:use manager interface
 	biliUps := upload.NewBiliUPLoads(inst.Config.BiliupConfigs, 2)
-	biliUps.Server(upload.RemoveFilesHandler)
+	go func() {
+		biliUps.Server(upload.RemoveFilesHandler)
+	}()
 	if err = metrics.NewCollector(ctx).Start(ctx); err != nil {
 		logger.Fatalf("failed to init metrics collector, error: %s", err)
 	}
